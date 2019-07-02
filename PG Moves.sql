e9d4f3304ae793953c5dc14b5eb7708e93045595
tim3426 <30452110+tim3426@users.noreply.github.com>
2019-07-02 13:17:33 -0700

    Update PG Moves.sql

--- PG Moves.sql
+++ PG Moves.sql
@@ -1,33 +1,20 @@
 
-select  distinct 	[C].[NAME] as [Prospect name],
-	[PLAN].[NAME] as [Plan name],
-	[I].[EXPECTEDDATE] as [Expected date],
-	[I].[ACTUALDATE] as [Actual date],
-	[I].[PROSPECTPLANSTEPSTAGE] as [Stage],
-	[I].[OBJECTIVE] as [Objective],
-	[I].[CATEGORY_TRANSLATION] as [Category],
-	[I].[SUBCATEGORY_TRANSLATION] as [Subcategory],
-	[I].[CONTACTMETHOD] as [Contact method],
-	[I].[STATUS] as [Status],
-	[I].[COMMENT] as [Comment],
-	case when [PLAN].[PROSPECTID] is null then 'Activity' else 'Move' end as [Action type],
-	[OWNER].[NAME] as [Owner],
-	[PROSPECTMANAGER].[NAME] as [Prospect manager],
-	case when [PG].[ID] is null then 'No' else 'Yes' end as [PG],
-	case when dateDiff(day, [I].[EXPECTEDDATE], getDate()) between 1 and 29 then 'Overdue 1+ days' 
-	     when dateDiff(day, [I].[EXPECTEDDATE], getDate()) between 30 and 59 then 'Overdue 30+ days'
-		 when dateDiff(day, [I].[EXPECTEDDATE], getDate()) between 60 and 120 then 'Overdue 60+ days' 
-		 when dateDiff(day, [I].[EXPECTEDDATE], getDate()) >=120 then 'Overdue 120+ days'
-		 else 'Scheduled'
-		 end as [Overdue?],
-	[I].[ID] as [INTERACTIONID],
-	[C].[ID] as [PROSPECTID],
-	[PLAN].[ID] as [PLANID],
-	[OWNER].[ID] as [OWNERID],
-	[PRIMARYMANAGER].[ID] as [PRIMARYMANAGERID],
-	[PROSPECTMANAGER].[ID] as [PROSPECTMANAGERID],
-	[C].[KEYNAME] as [SORT],
-[I].[ID] as [QUERYRECID]
+select  distinct 	[C].[NAME] as [Name]
+	,[C].[LookupID] as [Lookup ID]
+	,[OWNER].[NAME] as [Owner]
+	,[I].[ACTUALDATE] as [Date completed]
+	,CONVERT(varchar, [I].[DATECHANGED], 101) AS [Last modified]
+	,[I].[OBJECTIVE] as [Summary]
+	,[I].[CONTACTMETHOD] as [Contact method]
+	,[I].[CATEGORY_TRANSLATION] as [Category]
+	,[I].[SUBCATEGORY_TRANSLATION] as [Subcategory]
+	,[PROSPECTMANAGER].[NAME] as [Prospect manager]
+	,[PRIMARYMANAGER].[NAME] as [Primary manager]
+	,[PLAN].[NAME] as [Plan name]
+	,[I].[ID] as [Interaction ID]
+	,[C].[ID] as [Prospect ID]
+	,[PLAN].[ID] as [Plan ID]
+	,[I].[ID] as [QUERYRECID]
 from [dbo].[V_QUERY_INTERACTIONALL] as [I]
 left outer join [dbo].[V_QUERY_CONSTITUENT] as [C] on [I].[CONSTITUENTID] = [C].[ID]
 left outer join [dbo].[V_QUERY_PROSPECTPLAN] as [PLAN] on [I].[PROSPECTPLANID] = [PLAN].[ID]
@@ -35,7 +22,7 @@ left outer join [dbo].[V_QUERY_CONSTITUENT] as [OWNER] on [I].[OWNERID] = [OWNER
 left outer join [dbo].[V_QUERY_PROSPECT] as [P] on [PLAN].[PROSPECTID] = [P].[ID]
 left outer join [dbo].[V_QUERY_FUNDRAISER] as [PROSPECTMANAGER] on [P].[PROSPECTMANAGERFUNDRAISERID] = [PROSPECTMANAGER].[ID]
 left outer join [dbo].[V_QUERY_CONSTITUENT] as [PRIMARYMANAGER] on [PLAN].[PRIMARYMANAGERFUNDRAISERID] = [PRIMARYMANAGER].[ID]
-left join dbo.[UFN_ADHOCQUERYIDSET_F9007C17_434A_42AA_A4FD_5691B5FA0CF4]() as [PG] on [C].[ID] = [PG].ID
+--left outer join [dbo].[V_QUERY_INTERACTIONPARTICIPANT] as [Participants] on [I].[ID] = [Participants].[INTERACTIONID]
 left outer join (
 				select  distinct
 				 	min([P].[STARTDATE]) as [DATE],
@@ -48,8 +35,17 @@ left outer join (
 					[P].[ID], 
 					[PROSPECT].[ID]
 				) [MINPLAN] ON [P].[ID] = [MINPLAN].[PROSPECTID]
+left outer join
+	( 
+	select
+    [CONSTITUENT].[ID] [CONSTITUENTID],
+    [dbo].[UFN_BUSINESSUNITCODE_GETDESCRIPTION](ORGANIZATIONPOSITION.BUSINESSUNITCODEID) [BUSINESSUNIT]
+    from [dbo].[ORGANIZATIONHIERARCHY]
+    inner join dbo.ORGANIZATIONPOSITION on ORGANIZATIONPOSITION.ID = ORGANIZATIONHIERARCHY.ID
+    left outer join dbo.ORGANIZATIONPOSITIONHOLDER on ORGANIZATIONPOSITIONHOLDER.POSITIONID = ORGANIZATIONPOSITION.ID
+    left outer join dbo.CONSTITUENT on CONSTITUENT.ID = ORGANIZATIONPOSITIONHOLDER.CONSTITUENTID
+    ) [TEAM] on [TEAM].[CONSTITUENTID] = [OWNER].[ID]
 where  [I].[STATUS] = N'Completed'
-	and [PLAN].[ID] is not null
---	and ([I].[DATE] >= [MINPLAN].[DATE] or [I].[ACTUALDATE] >= [MINPLAN].[DATE] or [I].[EXPECTEDDATE] >= [MINPLAN].[DATE])
--- and [PLAN].[HISTORICAL] = 0 -- to remove
--- and [C].[NAME] = 'Energy Foundation'
+	and [PLAN].[PROSPECTID] is not null
+    and [TEAM].[BUSINESSUNIT] = 'Planned Gifts'
+	and [I].[ACTUALDATE] > DATEADD(MONTH,-12,GETDATE())
