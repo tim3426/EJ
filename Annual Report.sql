1196acdeb00151dd67d7f6244eb056ca33fcf487
tim3426 <30452110+tim3426@users.noreply.github.com>
2019-08-20 13:36:07 -0700

    lapsed = active

--- Annual Report.sql
+++ Annual Report.sql
@@ -37,7 +37,7 @@ case when [Anonymous].ID is null then ''
 ,C.[NICKNAME] as [Nickname]
 ,SpouseC.[NICKNAME] as [Spouse nickname]
 ,[AddSal].[PRIMARYADDRESSEE] as [Primary Addressee]
-,case when [Recognition].STATUS = 'Active' then 'Reviewed'
+,case when [Recognition].STATUS = 'Lapsed' then 'Active'
     else 'Pending'
     end as [Status]
 ,[Countable giving]
