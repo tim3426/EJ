d03790976fa5818586c05afd189ab81528c68ffe
tim3426 <30452110+tim3426@users.noreply.github.com>
2019-08-21 12:23:09 -0700

    remove recent research

--- Major gifts budgeting.sql
+++ Major gifts budgeting.sql
@@ -1,12 +1,32 @@
+USE [ejdevo]
+GO
+/****** Object:  StoredProcedure [dbo].[USP_DATALIST_ADHOCQUERY_A0D1B673_F4A5_497B_8113_9DAEB025E8B2]    Script Date: 21-Aug-19 12:20:14 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER procedure [dbo].[USP_DATALIST_ADHOCQUERY_A0D1B673_F4A5_497B_8113_9DAEB025E8B2](
+@PROSPECTMANAGER nvarchar(154) = null,
+	@CURRENTAPPUSERID uniqueidentifier,
+	@SECURITYFEATUREID uniqueidentifier,
+	@SECURITYFEATURETYPE tinyint,
+	@MAXROWS int = 100000) as
+set nocount on;
+set @PROSPECTMANAGER = dbo.UFN_SEARCHCRITERIA_GETLIKEPARAMETERVALUE2(@PROSPECTMANAGER, 0, null, 0);
 declare @FISCALYEAR nvarchar(25) = convert(nvarchar(25),case when month(getDate()) > 6 then year(getDate())+1 else year(getDate())end);
 declare @ENDDATE date = cast(@FISCALYEAR + '-' + '06' + '-' + '30' as date)
 declare @STARTDATE date = cast(cast(convert(int,@FISCALYEAR) - 1 as nvarchar) + '-'+ '07' + '-' + '01' as date);
+
+
+with
+[ROOT_CTE] as (
 
 select distinct C.CONSTITUENTID as [PROSPECTID]
 	,C.LOOKUPID [Lookup ID]
 	,C.[SORT_NAME] as [Sort]
 	,C.[NAME] [Name]
-	,C.PRIMARY_CONSTITUENCY
+	,C.PRIMARY_CONSTITUENCY as [Constituency]
 	,C.PROSPECT_MANAGER AS [Prospect Manager]
 	,[PRIMARY].[NAME] [Primary Manager]
 	,[PLAN].[NAME] as [Plan]
@@ -20,8 +40,8 @@ select distinct C.CONSTITUENTID as [PROSPECTID]
 	,O.EXPECTEDASKAMOUNT as [Accrual Budget]
 	,BUDGET.[ACCRUAL_REFORECAST] as [Reforecast Accrual Budget]
 	,BUDGET.[CASH_REFORECAST] as [Reforecast Cash Budget]
-	,BUDGET.[ASKAMOUNT] as [Ask Amount static]
-	,BUDGET.ASKDATE as [Ask Date static]
+	,BUDGET.[ASKAMOUNT] as [Ask Amount]
+	,BUDGET.ASKDATE as [Ask Date]
 	,O.[ASKAMOUNT] as [Ask Amount actual]
 	,O.[ASKDATE] as [Ask Date actual]
 	,PLANS_RESTRICTION.RESTRICTIONS [Restrictions]
@@ -37,8 +57,8 @@ select distinct C.CONSTITUENTID as [PROSPECTID]
 	,C.EJ_AFFINITY as [Affinity]
 	,C.EJ_EST_WEALTH as [Estimated Wealth]
 	,C.EJ_INCLINATION as [Inclination]
-	,RECENTRESEARCH.VALUE as [Last Research Type]
-	,RECENTRESEARCH.STARTDATE as [Last Research Date]
+	--,RECENTRESEARCH.VALUE as [Last Research Type]
+	--,RECENTRESEARCH.STARTDATE as [Last Research Date]
 	,LASTMOVE.[Objective] as [Last Move]
 	,LASTMOVE.[ACTUALDATE] as [Last Move Date]
 	,LASTMOVE.[OWNERID] as [Last Move Owner]
@@ -51,6 +71,7 @@ select distinct C.CONSTITUENTID as [PROSPECTID]
 	,ADSAL.[PRIMARYSALUTATION] as [Salutation]
 	,C.EMAIL as [Email]
 	,C.PROSPECT_MANAGER_TEAM [Team]
+	,[Primary].CONSTITUENTID as [PRIMARYID]
 	,O.[BASECURRENCYID] as [Revenue\Base currency ID]
 	,O.[BASECURRENCYID] as [Revenue\Base currency ID 1]
 	,O.[BASECURRENCYID] as [Revenue\Base currency ID 2]
@@ -71,8 +92,10 @@ from EJ_CONSTITUENT_HISTORY as C
 	left join EJ_CONSTITUENT_HISTORY as [Primary] on [Primary].CONSTITUENTID = [PLAN].PRIMARYMANAGERFUNDRAISERID
 	--left outer join [dbo].[USR_V_QUERY_PROSPECTPLANSTEPS] as LASTMOVE on [PLAN].[ID] = [LASTMOVE].[ID]
 	left outer join V_QUERY_ADDRESSEE_SALUTATION ADSAL on ADSAL.ID = C.CONSTITUENTID
-	left outer join [dbo].[V_QUERY_ATTRIBUTE6920A0A601734A38A098EE001E5D02F0] as [RECENTRESEARCH] on C.CONSTITUENTID = [RECENTRESEARCH].ID
-
+	/** this table is fundamentally broken
+	--left outer join [dbo].[V_QUERY_ATTRIBUTE6920A0A601734A38A098EE001E5D02F0] as [RECENTRESEARCH] on C.CONSTITUENTID = [RECENTRESEARCH].ID 
+	**/
+	
 	left join 
 			(select *
 			from
@@ -157,3 +180,60 @@ where C.PROSPECTSTATUS in (
 			)
 and  (C.PROSPECT_MANAGER_TEAM = 'Major Gifts'
 	OR PRIMARYTEAM.BUSINESSUNIT = 'Major Gifts')
+
+
+)
+
+
+select top(@MAXROWS) [PROSPECTID],
+	[Lookup ID],
+	[Sort],
+	[Name],
+	[Constituency],
+	[Prospect Manager],
+	[PRIMARYID],
+	[Primary Manager],
+	[Plan],
+	[Status],
+	[Narrative],
+	[Open Pledge],
+	[Bookable],
+	[Original accrual budget],
+	[Original cash budget],
+	[Reforecast Accrual Budget],
+	[Reforecast cash budget],
+	[Ask amount],
+	[Ask Date],
+	[Restrictions],
+	[Current Fiscal Giving],
+	[Prior Fiscal Giving],
+	[Two Years Prior Fiscal Giving],
+	[Total Giving],
+	[PLANID],
+	[OPPORTUNITYID],
+	[City],
+	[State],
+	[5 Year Capacity],
+	[Affinity],
+	[Estimated Wealth],
+	[Inclination],
+	--[Last Research Type],
+	--[Last Research Date],
+	[Last Move],
+	[Last Move Date],
+	[Last Move Owner],
+	[Next Move],
+	[Next Move Date],
+	[Next Move Owner],
+	[Address],
+	[Zip],
+	[Addressee],
+	[Salutation],
+	[Email],
+	[Team],
+	[Revenue\Base currency ID],
+	[Prospect\Prospect Plans\Opportunities\BASECURRENCYID]
+from [ROOT_CTE] as QUERYRESULTS
+where ((@PROSPECTMANAGER is null or @PROSPECTMANAGER = '') or QUERYRESULTS.[Prospect Manager] LIKE  '%' + @PROSPECTMANAGER + '%')
+
+
